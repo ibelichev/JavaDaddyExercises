8c1ebb8dd40d63f68b68c4560790c8a60c1db884
ibelichev <belichev1102@gmail.com>
2025-07-18 13:17:55 +0300

    1

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task2_MathUtils/mySolution/MathUtilsDemo.java
@@ -0,0 +1,7 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task2_MathUtils.mySolution;
+
+public class MathUtilsDemo {
+    public static void main(String[] args) {
+        System.out.println(MathUtils.max(10, 1392));
+    }
+}
